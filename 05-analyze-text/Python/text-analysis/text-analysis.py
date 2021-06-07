51b539e3f7e33dc765ad19faab2932746ca18186
Daniel Peter <58658723+danielpeter98@users.noreply.github.com>
2021-06-06 22:04:39 -0700

    Add files via upload

---
+++ 05-analyze-text/Python/text-analysis/text-analysis.py
@@ -0,0 +1,62 @@
+from dotenv import load_dotenv
+import os
+
+# Import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+
+def main():
+    try:
+        # Get Configuration Settings
+        load_dotenv()
+        cog_endpoint = os.getenv('COG_SERVICE_ENDPOINT')
+        cog_key = os.getenv('COG_SERVICE_KEY')
+
+        # Create client using endpoint and key
+        credential = AzureKeyCredential(cog_key)
+        cog_client = TextAnalyticsClient(endpoint=cog_endpoint, credential=credential)
+
+        # Analyze each text file in the reviews folder
+        reviews_folder = 'reviews'
+        for file_name in os.listdir(reviews_folder):
+            # Read the file contents
+            print('\n-------------\n' + file_name)
+            text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
+            print('\n' + text)
+
+            # Get language
+            detectedLanguage = cog_client.detect_language(documents=[text])[0]
+            print('\nLanguage: {}'.format(detectedLanguage.primary_language.name))
+
+            # Get sentiment
+            sentimentAnalysis = cog_client.analyze_sentiment(documents=[text])[0]
+            print("\nSentiment: {}".format(sentimentAnalysis.sentiment))
+
+            # Get key phrases
+            phrases = cog_client.extract_key_phrases(documents=[text])[0].key_phrases
+            if len(phrases) > 0:
+                print("\nKey Phrases:")
+                for phrase in phrases:
+                    print('\t{}'.format(phrase))
+
+            # Get entities
+            entities = cog_client.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nEntities")
+                for entity in entities:
+                    print('\t{} ({})'.format(entity.text, entity.category))
+
+            # Get linked entities
+            entities = cog_client.recognize_linked_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nLinks")
+                for linked_entity in entities:
+                    print('\t{} ({})'.format(linked_entity.name, linked_entity.url))
+
+
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 07-speech/Python/speaking-clock/speaking-clock.py
@@ -0,0 +1,86 @@
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+
+# Import namespaces
+# Import namespaces
+import azure.cognitiveservices.speech as speech_sdk
+
+def main():
+    try:
+        global speech_config
+
+        # Get Configuration Settings
+        load_dotenv()
+        cog_key = os.getenv('COG_SERVICE_KEY')
+        cog_region = os.getenv('COG_SERVICE_REGION')
+
+        # Configure speech service
+         # Configure speech service
+        speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
+        print('Ready to use speech service in:', speech_config.region)
+
+        # Get spoken input
+        command = TranscribeCommand()
+        if command.lower() == 'what time is it?':
+            TellTime()
+
+    except Exception as ex:
+        print(ex)
+
+def TranscribeCommand():
+    command = ''
+
+    # Configure speech recognition
+    # Configure speech recognition
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+    print('Speak now...')
+
+    # Process speech input
+    # Process speech input
+    speech = speech_recognizer.recognize_once_async().get()
+    if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+        command = speech.text
+        print(command)
+    else:
+        print(speech.reason)
+        if speech.reason == speech_sdk.ResultReason.Canceled:
+            cancellation = speech.cancellation_details
+            print(cancellation.reason)
+            print(cancellation.error_details)
+
+    # Return the command
+    return command
+
+
+def TellTime():
+    now = datetime.now()
+    response_text = 'The time is {}:{:02d}'.format(now.hour,now.minute)
+
+
+    # Configure speech synthesis
+    # Configure speech synthesis
+    speech_config.speech_synthesis_voice_name = 'en-GB-George' # add this
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+
+    # Synthesize spoken output
+    # Synthesize spoken output
+    responseSsml = " \
+        <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'> \
+            <voice name='en-GB-Susan'> \
+                {} \
+                <break strength='weak'/> \
+                Time to end this lab! \
+            </voice> \
+        </speak>".format(response_text)
+    speak = speech_synthesizer.speak_ssml_async(responseSsml).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
+
+    # Print the response
+    print(response_text)
+
+
+if __name__ == "__main__":
+    main()
